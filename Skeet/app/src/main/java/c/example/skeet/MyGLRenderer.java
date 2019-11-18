7dcb6ef3b8e496409b0c9ac96ec5a884f1925e74
Leonardo Silva <coelho_ll@hotmail.com>
2019-11-18 14:57:50 -0700

    added projection
    change color of triangle

--- Skeet/app/src/main/java/c/example/skeet/MyGLRenderer.java
+++ Skeet/app/src/main/java/c/example/skeet/MyGLRenderer.java
@@ -2,6 +2,7 @@ package c.example.skeet;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import javax.microedition.khronos.egl.EGLConfig;
 
@@ -11,6 +12,10 @@ class MyGLRenderer implements GLSurfaceView.Renderer {
 
 
     private  Triangle triangle;
+
+    private final float[] vPMatrix = new float[16];
+    private final float[] projectionMatrix = new float[16];
+    private final float[] viewMatrix = new float[16];
 
     //Shaders contain OpenGL Shading Language (GLSL) code that
     // must be compiled prior to using it in the OpenGL ES environment.
@@ -41,10 +46,29 @@ class MyGLRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 unused) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        triangle.draw();
-    }
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(viewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
 
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+        // Draw shape
+        triangle.draw(vPMatrix);
+
+
+    }
+    // vPMatrix is an abbreviation for "Model View Projection Matrix"
+
+
+    @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
+
 }
--- Skeet/app/src/main/java/c/example/skeet/Triangle.java
+++ Skeet/app/src/main/java/c/example/skeet/Triangle.java
@@ -19,12 +19,20 @@ public class Triangle {
 
 
 
-   // this is a function to draw
     private final String vertexShaderCode =
-            "attribute vec4 vPosition;" +
+            // This matrix member variable provides a hook to manipulate
+            // the coordinates of the objects that use this vertex shader
+            "uniform mat4 uMVPMatrix;" +
+                    "attribute vec4 vPosition;" +
                     "void main() {" +
-                    "  gl_Position = vPosition;" +
+                    // the matrix must be included as a modifier of gl_Position
+                    // Note that the uMVPMatrix factor *must be first* in order
+                    // for the matrix multiplication product to be correct.
+                    "  gl_Position = uMVPMatrix * vPosition;" +
                     "}";
+
+    // Use to access and set the view transformation
+    private int vPMatrixHandle;
 
 
     // that is a function to color it
@@ -45,9 +53,9 @@ public class Triangle {
     };
 
     // Set color with red, green, blue and alpha (opacity) values
-    float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
+    float color[] = { 1.0f, 1.0f, 0.9f, 1.0f };
 
-    public void draw() {
+    public void draw(float[] mvpMatrix) {
         // Add program to OpenGL ES environment
         GLES20.glUseProgram(mProgram);
 
@@ -67,6 +75,19 @@ public class Triangle {
 
         // Set color for drawing the triangle
         GLES20.glUniform4fv(colorHandle, 1, color, 0);
+
+        // Draw the triangle
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(positionHandle);
+
+
+        // get handle to shape's transformation matrix
+        vPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+
+        // Pass the projection and view transformation to the shader
+        GLES20.glUniformMatrix4fv(vPMatrixHandle, 1, false, mvpMatrix, 0);
 
         // Draw the triangle
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
