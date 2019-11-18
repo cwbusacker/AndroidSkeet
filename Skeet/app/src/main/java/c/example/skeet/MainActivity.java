0b2a85ef0c25a72c74ce867fcbde45062728d365
Leonardo Silva <coelho_ll@hotmail.com>
2019-11-18 14:43:39 -0700

    created the openGL environment
    display triangle

--- Skeet/app/src/main/java/c/example/skeet/MainActivity.java
+++ Skeet/app/src/main/java/c/example/skeet/MainActivity.java
@@ -1,13 +1,24 @@
 package c.example.skeet;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GLSurfaceView gLView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        gLView = new MyGLSurfaceView(this);
+
+        setContentView(gLView);
     }
 }
---
+++ Skeet/app/src/main/java/c/example/skeet/MyGLSurfaceView.java
@@ -0,0 +1,22 @@
+package c.example.skeet;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+class MyGLSurfaceView extends GLSurfaceView {
+
+        private final MyGLRenderer renderer;
+
+    public MyGLSurfaceView(Context context){
+            super(context);
+
+            // Create an OpenGL ES 2.0 context
+            setEGLContextClientVersion(2);
+
+            renderer = new MyGLRenderer();
+
+            // Set the Renderer for drawing on the GLSurfaceView
+            setRenderer(renderer);
+        }
+
+}
